621610b199227c337285085e45554ad2ae35602c
vladomatula <matula.vl96@gmail.com>
2022-05-22 21:40:05 +0200

    Initialize servers and resolvers

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import {ChatResolver} from './resolvers/chat';
+
+dotenv.config({path: 'conf/.env'});
+
+const main = async () => {
+  const app = express();
+
+  app.use(cors({origin: '*', credentials: false}));
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [ChatResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `Server ready at http://localhost:${process.env.PORT}${apolloServer.graphqlPath}`
+    );
+  });
+};
+
+
+main().catch((err) => {
+  console.log(err);
+});
